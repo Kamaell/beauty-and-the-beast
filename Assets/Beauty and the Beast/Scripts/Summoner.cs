9f6bb8fdc354788a15bc47e4087e67d609422f53
Kamael <bogumil.mazurek@gmail.com>
2020-03-04 11:41:52 +0100

    Update for 2019.3.1

--- Assets/Beauty and the Beast/Scripts/Summoner.cs
+++ Assets/Beauty and the Beast/Scripts/Summoner.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.VFX;
+using UnityEngine.VFX;
 
 public class Summoner : MonoBehaviour
 {
